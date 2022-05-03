8461fc5994e85d1b74de4e9fbc4be4f4a44dc4b3
Ekaterina <kogdato5@gmail.com>
2022-05-03 13:13:25 +0300

    Initial Commit

---
+++ Entity+CoreDataProperties.swift
@@ -0,0 +1,23 @@
+
+
+import Foundation
+import CoreData
+
+
+extension Entity {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Entity> {
+        return NSFetchRequest<Entity>(entityName: "Entity")
+    }
+
+    @NSManaged public var date: Date?
+    @NSManaged public var name: String?
+    @NSManaged public var rep: Int16
+    @NSManaged public var reps: Int16
+    @NSManaged public var weight: Int16
+
+}
+
+extension Entity : Identifiable {
+
+}
