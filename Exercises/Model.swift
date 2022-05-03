ce23aeb2b29246d62235d95bdeca4e6b6c283c4f
Ekaterina <kogdato5@gmail.com>
2022-05-03 18:54:49 +0300

    tableview

--- Exercises/Model.swift
+++ Exercises/Model.swift
@@ -1,8 +1,38 @@
 
 
 import Foundation
+import CoreData
 
 
-var exersizes = [ "Отведение ног", "Квадрицепс ног",
-                  "Бицепс тренажер", "Пресс тренажер",
-                  "Спина гиперэкстензия"]
+
+
+let context = AppDelegate().persistentContainer.viewContext
+
+
+class DataModel{
+    
+    var exersizes = [ "Отведение ног", "Квадрицепс ног",
+                      "Бицепс тренажер", "Пресс тренажер",
+                      "Спина гиперэкстензия"]
+    
+
+    func addModel(){
+        let newItem = Entity(context: context)
+        newItem.date = Date()
+        newItem.name = ""
+        newItem.rep = 0
+        newItem.reps = 10
+        newItem.weight = 10
+        saveModel()
+    }
+
+    func saveModel(){
+     
+        do {
+            try context.save()
+        } catch {
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        }
+    }
+}
--- Exercises/ViewController.swift
+++ Exercises/ViewController.swift
@@ -1,14 +1,89 @@
 
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    let cellId = "cellId"
+    var exercises: [NSManagedObject] = []
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Entity")
+            do {
+                exercises = try context.fetch(fetchRequest)
+                print(exercises)
+            } catch let err as NSError {
+                print(err)
+            }
+            tableView.reloadData()
+    }
+//    var exerciss: FetchedResults<Entity>
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let tableView = UITableView()
+        view.addSubview(tableView)
+        setupTableView()
+        self.tableView.contentInset = UIEdgeInsets(top: 70, left: 0, bottom: 0, right: 0)
+        setupButton()
+        
     }
-
-
+    
+    func setupTableView(){
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+    }
+    
+    func setupButton(){
+        let button = UIButton(frame: CGRect(x: 40, y: 100, width: UIScreen.main.bounds.width - 80, height: 50))
+        button.backgroundColor = .systemIndigo
+        button.setTitle("Добавить", for: .normal)
+        button.addTarget(self, action: #selector(addItem), for: .touchUpInside)
+        
+        self.view.addSubview(button)
+        button.frame.origin.y = -60
+    }
+    
+    @objc func addItem(){
+        DataModel().addModel()
+        tableView.reloadData()
+        print(exercises)
+    }
+    
 }
 
+extension ViewController {
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return exercises.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        let currentEx = exercises[indexPath.row]
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.YY"
+
+        let dateText = dateFormatter.string(from: (currentEx.value(forKey: "date") as! Date))
+        print(dateText)
+        cell.textLabel?.text = dateText
+        
+    
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+            return 5
+        }
+
+    override func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+            return UIView()
+        }
+}
